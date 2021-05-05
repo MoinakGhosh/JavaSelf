ab35c6da311727e9bec0374e903da9de6f6370e2
MoinakGhosh <79351755+MoinakGhosh@users.noreply.github.com>
2021-05-05 17:05:15 +0530

    Add files via upload

--- programs/MortageCalculator.java
+++ programs/MortageCalculator.java
@@ -1,1 +1,25 @@
 
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+public class MortageCalculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the details below.");
+        System.out.print("Principal : Rs.");
+        int principal = sc.nextInt();
+        System.out.print("Annual Interest Rate : ");
+        double rate = sc.nextDouble();
+        System.out.print("Period (Years) : ");
+        int period = sc.nextInt();
+        int tn=period*12; //total number of months
+        double nr = rate/1200; //monthly rate
+        double mortage = (double)(principal * (nr*(Math.pow(1+nr,tn)))/((Math.pow(1+nr,tn))-1));
+        System.out.println("Mortage Value : Rs." + mortage);
+
+    }
+}
